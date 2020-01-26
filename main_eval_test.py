cbd8bd644dbe4358445ebfbd35043a64e90002f3
Kunal Katarya <kunal.rox@gmail.com>
2020-01-26 16:42:50 +0000

    qn 3.1

--- main_eval_test.py
+++ main_eval_test.py
@@ -3,61 +3,69 @@ import numpy as np
 from dataset import Dataset
 from classification import DecisionTreeClassifier
 
-'''
-p = np.array(["A","B","A","A","B","C","A","C","C","A","B","C"])
-a = np.array(["B","B","C","B","A","A","B","A","C","B","C","C"])
-
-sort = ["C", "B", "A"]
-
-c = e.confusion_matrix(p,a)
-print(e.accuracy(c))
-print(e.precision(c))
-print(e.recall(c))
-print(e.f1_score(c))
-'''
-
 full_dataset_path = "./data/train_full.txt"
 subset_path = "./data/train_sub.txt"
 noisy_path = "./data/train_noisy.txt"
 test_path = "./data/test.txt"
+val_path = "./data/validation.txt"
 
-#load dataset, atttribs, labels
-d_subset = Dataset()
-d_subset.initFromFile(subset_path)
-attribs = d_subset.attrib
-labels = d_subset.labels
+class q3_1:
+    def calc_stats(self, test_path, path_to_data):
+        #load dataset, atttribs, labels
+        d_subset = Dataset()
+        d_subset.initFromFile(path_to_data)
+        attribs = d_subset.attrib
+        labels = d_subset.labels
 
-ds_test = Dataset()
-ds_test.initFromFile(test_path)
-test_attribs = ds_test.attrib
-test_labels = ds_test.labels
+        ds_test = Dataset()
+        ds_test.initFromFile(test_path)
+        test_attribs = ds_test.attrib
+        test_labels = ds_test.labels
 
-#train and predict
-tree = DecisionTreeClassifier()
-tree.train(attribs, labels)
-predictions = tree.predict(test_attribs)
+        #train and predict
+        tree = DecisionTreeClassifier()
+        tree.train(attribs, labels)
+        predictions = tree.predict(test_attribs)
 
-evaluator = Evaluator()
-c_matrix = evaluator.confusion_matrix(predictions, test_labels)
+        evaluator = Evaluator()
+        c_matrix = evaluator.confusion_matrix(predictions, test_labels)
 
-print("Accuracy: " + str(evaluator.accuracy(c_matrix)))
+        print(" ")
+        print("Accuracy: " + str(evaluator.accuracy(c_matrix)))
+        print(" ")
+
+        precision, macro_p = evaluator.precision(c_matrix)
+        recall, macro_r = evaluator.recall(c_matrix)
+        f1, macro_f1 = evaluator.f1_score(c_matrix)
+
+        unique_lables = np.sort(np.unique(test_labels))
+
+        total = np.column_stack((unique_lables,precision, recall, f1))
+        header_row = np.array([" ", "Prec.", "Recall", "F1"])
+        final_table = np.vstack((header_row, total))
+
+        print(final_table)
+
+        '''
+        print("Precision: " + str(precision))
+        print("Recall: " + str(recall))
+        print("F1 Score: " + str(f1))'''
+
+        print(" ")
+        print("Macro avg recall:" + str(macro_r))
+        print("Macro avg precision:" + str(macro_p))
+        print("Macro avg f1:" + str(macro_f1))
+        print(" ")
 
 
-precision, macro_p = evaluator.precision(c_matrix)
-recall, macro_r = evaluator.recall(c_matrix)
-f1, macro_f1 = evaluator.f1_score(c_matrix)
+print("FULL DATASET: ")
+full_3_1 = q3_1()
+full_3_1.calc_stats(test_path, full_dataset_path)
 
-total = np.column_stack((precision, recall, f1))
-print(total)
+print("SUBSET: ")
+sub_3_1 = q3_1()
+sub_3_1.calc_stats(test_path, subset_path)
 
-
-print("Precision: " + str(precision))
-print("Recall: " + str(recall))
-print("F1 Score: " + str(f1))
-
-print("Macro avg recall:" + str(macro_r))
-print("Macro avg precision:" + str(macro_p))
-print("Macro avg f1:" + str(macro_f1))
-
-
-e = Evaluator()
+print("NOISY: ")
+noisy_3_1 = q3_1()
+noisy_3_1.calc_stats(test_path, noisy_path)
