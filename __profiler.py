a2a6d148419cb71733367c7a97282b733ef62008
Lin Hao Lee <lhlee888@hotmail.com>
2020-01-27 18:36:03 +0000

    remove profiler

--- __profiler.py
+++ __profiler.py
@@ -1,38 +1,38 @@
-import numpy as np
-import dataset as ds
-import classification as cs
-import prune
-import cProfile
+# import numpy as np
+# import dataset as ds
+# import classification as cs
+# import prune
+# import cProfile
 
-pathToSimple1 = './data/simple1.txt'
-pathToSimple2 = './data/simple2.txt'
-pathToTest = './data/test.txt'
-pathToToy = './data/toy.txt'
-pathToToy2 = './data/toy2.txt'
-pathToFull = './data/train_full.txt'
-pathToNoisy = './data/train_noisy.txt'
-pathToSub = './data/train_sub.txt'
-pathToValid = './data/validation.txt'
+# pathToSimple1 = './data/simple1.txt'
+# pathToSimple2 = './data/simple2.txt'
+# pathToTest = './data/test.txt'
+# pathToToy = './data/toy.txt'
+# pathToToy2 = './data/toy2.txt'
+# pathToFull = './data/train_full.txt'
+# pathToNoisy = './data/train_noisy.txt'
+# pathToSub = './data/train_sub.txt'
+# pathToValid = './data/validation.txt'
 
-dataset = ds.ClassifierDataset()
-dataset.initFromFile(pathToFull)
+# dataset = ds.ClassifierDataset()
+# dataset.initFromFile(pathToFull)
 
-dtc = cs.DecisionTreeClassifier()
-print("FULL")
-cProfile.run('dtc.train(dataset.attrib, dataset.labels)', None, 'time')
-cProfile.run('dtc.train(dataset.attrib, dataset.labels)', None, 'time')
-cProfile.run('dtc.train(dataset.attrib, dataset.labels)', None, 'time')
+# dtc = cs.DecisionTreeClassifier()
+# print("FULL")
+# cProfile.run('dtc.train(dataset.attrib, dataset.labels)', None, 'time')
+# cProfile.run('dtc.train(dataset.attrib, dataset.labels)', None, 'time')
+# cProfile.run('dtc.train(dataset.attrib, dataset.labels)', None, 'time')
 
-dataset.initFromFile(pathToSub)
-print("=====")
-print("SUB")
-cProfile.run('dtc.train(dataset.attrib, dataset.labels)', None, 'time')
-cProfile.run('dtc.train(dataset.attrib, dataset.labels)', None, 'time')
-cProfile.run('dtc.train(dataset.attrib, dataset.labels)', None, 'time')
+# dataset.initFromFile(pathToSub)
+# print("=====")
+# print("SUB")
+# cProfile.run('dtc.train(dataset.attrib, dataset.labels)', None, 'time')
+# cProfile.run('dtc.train(dataset.attrib, dataset.labels)', None, 'time')
+# cProfile.run('dtc.train(dataset.attrib, dataset.labels)', None, 'time')
 
-dataset.initFromFile(pathToNoisy)
-print("=====")
-print("NOISY")
-cProfile.run('dtc.train(dataset.attrib, dataset.labels)', None, 'time')
-cProfile.run('dtc.train(dataset.attrib, dataset.labels)', None, 'time')
-cProfile.run('dtc.train(dataset.attrib, dataset.labels)', None, 'time')
+# dataset.initFromFile(pathToNoisy)
+# print("=====")
+# print("NOISY")
+# cProfile.run('dtc.train(dataset.attrib, dataset.labels)', None, 'time')
+# cProfile.run('dtc.train(dataset.attrib, dataset.labels)', None, 'time')
+# cProfile.run('dtc.train(dataset.attrib, dataset.labels)', None, 'time')
