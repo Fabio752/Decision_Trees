6ed9c246f94b81304a1cd528e5f654bc1a2b7b56
Kunal Katarya <kunal.rox@gmail.com>
2020-02-04 03:53:22 +0000

    __main

--- __main.py
+++ __main.py
@@ -17,20 +17,12 @@ pathToExample = './data/example.txt'
 
 
 dataset = ClassifierDataset()
-dataset.initFromFile(pathToFull)
+dataset.initFromFile(pathToSub)
 
 dtc = DecisionTreeClassifier()
-# dtc.train(dataset.attrib, dataset.labels)
-dtc.readFromFile('./memes.blob')
-# print(dtc.is_trained)
-# print(dtc.classifierTree)
-# dtc.writeToFile('./memes.blob')
-# print(dtc.is_trained)
+dtc.train(dataset.attrib, dataset.labels)
+dtc.writeToFile('model_0.pickle')
 
-
-# validationDataset = ClassifierDataset()
-# validationDataset.initFromFile(pathToValid)
-
-# Prune(dtc, validationDataset.attrib, validationDataset.labels)
-
+dtc2 = DecisionTreeClassifier()
+dtc2.readFromFile('model_0.pickle')
 print(dtc.__repr__(5))
